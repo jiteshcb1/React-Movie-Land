6c0244f8cb4eaab9458387a193f2efed47532a6d
jiteshcb1 <jitesh.chhabra@outlook.com>
2022-05-02 11:16:56 +0530

    Updates

---
+++ precache-manifest.c4b7aade551a0883c8c4c755833ab60f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbb5905c9df9e8c6dd83",
+    "url": "/React-Movie-Land/static/css/main.afb9b9a3.chunk.css"
+  },
+  {
+    "revision": "fbb5905c9df9e8c6dd83",
+    "url": "/React-Movie-Land/static/js/main.fbb5905c.chunk.js"
+  },
+  {
+    "revision": "3a77647f381334bbd57d",
+    "url": "/React-Movie-Land/static/js/1.3a77647f.chunk.js"
+  },
+  {
+    "revision": "29e76b581fae4f415299",
+    "url": "/React-Movie-Land/static/js/runtime~main.29e76b58.js"
+  },
+  {
+    "revision": "8ba3b3d5526afbf16fdfb4e8ecbe0e7c",
+    "url": "/React-Movie-Land/static/media/search.8ba3b3d5.svg"
+  },
+  {
+    "revision": "476f06753ced45a01d2e554e7629c259",
+    "url": "/React-Movie-Land/index.html"
+  }
+];
